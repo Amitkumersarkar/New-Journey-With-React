273df2c50ea5ba17174a904ffd6f6e89e472c497
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-01-27 04:31:30 +0600

    added new jsx file

---
+++ explore-react-core-concepts-part02/src/team.jsx
@@ -0,0 +1,13 @@
+export default function team() {
+    const teamStyle = {
+        border: '2px solid red',
+        margin: '15px',
+        padding: '15px',
+        borderRadius: '15px'
+    }
+    return (
+        <div style={teamStyle}>
+            <h3>Players : </h3>
+        </div>
+    )
+}
