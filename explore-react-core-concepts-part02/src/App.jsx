4d42a91136703f57dc794299b5ea45a6e2bf7804
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-01-27 01:47:44 +0600

    added new file

--- explore-react-core-concepts-part02/src/App.jsx
+++ explore-react-core-concepts-part02/src/App.jsx
@@ -2,9 +2,29 @@ import './App.css'
 
 function App() {
 
+  // create an normal function
+  function handleClick() {
+    alert('btn clicked');
+  }
+
+  // create an arrow function
+
+  const handleClick2 = () => {
+    alert('btn 2 is clicked');
+  }
+
+  // parameterized function
+
+  const addToFive = (num) => {
+    alert(num + 5);
+  }
+
   return (
     <>
       <h3>React Core Concepts</h3>
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={handleClick2}>Click Me 2</button>
+      <button onClick={addToFive(2)}>Click Me 2</button>
     </>
   )
 }
