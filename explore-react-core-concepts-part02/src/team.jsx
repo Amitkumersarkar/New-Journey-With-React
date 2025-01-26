c4667876239be608b95d5c494f36a4c9a3bfeb85
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-01-27 04:43:45 +0600

    added team jsx file

--- explore-react-core-concepts-part02/src/team.jsx
+++ explore-react-core-concepts-part02/src/team.jsx
@@ -1,4 +1,20 @@
+import { useState } from "react"
+
 export default function team() {
+
+    // create an state here
+    const [team, setTeam] = useState(11);
+
+    const handleAdd = () => {
+        const newTeam = team + 1;
+        setTeam(newTeam);
+    }
+
+    const handleRemove = () => {
+        // remove team
+        setTeam(team - 1);
+    }
+
     const teamStyle = {
         border: '2px solid red',
         margin: '15px',
@@ -7,7 +23,8 @@ export default function team() {
     }
     return (
         <div style={teamStyle}>
-            <h3>Players : </h3>
+            <h3>Players :{team}</h3>
+            <button onClick={handleAdd}>Add</button>
         </div>
     )
 }
