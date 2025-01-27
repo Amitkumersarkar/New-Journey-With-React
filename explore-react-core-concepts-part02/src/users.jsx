124cfea0c97fab4bc20516dfcb423e5984ffd94d
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-01-27 07:22:57 +0600

    added useeffect file

--- explore-react-core-concepts-part02/src/users.jsx
+++ explore-react-core-concepts-part02/src/users.jsx
@@ -1,8 +1,13 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export default function Users() {
     // declared an state
     const [users, setUsers] = useState([]);
+    // to load data using useEffect function with dependency
+    useEffect(() => {
+
+    }, [])
+
     return (
         <div>
 
